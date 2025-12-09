32f869adbf9e86db74165f9b3649ff33b8c9f902
prachipatel2204-ops <prachipatel2204@gmail.com>
2025-12-09 14:47:17 +0530

    Change in app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   //templateUrl: './app.html',
   //styleUrl: './app.css'
-  template:`<h1>Hello</h1> <h2>Hi</h2> <h3>How Are You</h3> <h4>Fine</h4>`,
+  template:`<marquee><h1>Hello</h1> <h2>Hi</h2> <h3>How Are You</h3> <h4>Fine</h4></marquee>`,
   styles:`h1{color:blue;font-size:80px} h2{color:green;font-size:70px} h3{color:red;font-size:60px} h4{color:yellow;font-size:50px}`
 })
 export class App {
