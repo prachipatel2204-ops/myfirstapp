1d206931c18b82f1da88a94f3355425e7cba5e3e
prachipatel2204-ops <prachipatel2204@gmail.com>
2025-12-12 18:33:27 +0530

    databinding example in admin-contact component

--- src/app/admin/admin-contact/admin-contact.ts
+++ src/app/admin/admin-contact/admin-contact.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './admin-contact.html',
   styleUrl: './admin-contact.css',
 })
 export class AdminContact {
-
+protected name="Contact Page"
+protected username?:String;
+protected studname=signal("Patel Prachi");
+imageUrl="images/reg.jpg"
 }
