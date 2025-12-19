e649e87330dc68f997acd49bc4561aa4fc348592
prachipatel2204-ops <prachipatel2204@gmail.com>
2025-12-19 14:33:50 +0530

    Edit admin-contact

--- src/app/admin/admin-contact/admin-contact.ts
+++ src/app/admin/admin-contact/admin-contact.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Component,OnInit,DoCheck,AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-contact',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './admin-contact.html',
   styleUrl: './admin-contact.css',
 })
-export class AdminContact {
+export class AdminContact
+  implements OnInit, DoCheck, AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked {
 
   name = '';
   email = '';
@@ -16,16 +18,44 @@ export class AdminContact {
   contact: any[] = [];
   editIndex: number | null = null;
 
+  // Called once when the component is created
+ngOnInit() {
+  console.log('AdminContact ngOnInit');
+}
+
+// Called every time Angular checks for changes
+ngDoCheck() {
+  console.log('AdminContact ngDoCheck');
+}
+
+// Called once after external content is loaded
+ngAfterContentInit() {
+  console.log('AdminContact ngAfterContentInit');
+}
+
+// Called every time after content is checked
+ngAfterContentChecked() {
+  console.log('AdminContact ngAfterContentChecked');
+}
+
+// Called once after component view and child views are loaded
+ngAfterViewInit() {
+  console.log('AdminContact ngAfterViewInit');
+}
+
+// Called every time after the view(html) is checked
+ngAfterViewChecked() {
+  console.log('AdminContact ngAfterViewChecked');
+}
+
   add() {
     if (this.editIndex === null) {
-      // Add New
       this.contact.push({
         name: this.name,
         email: this.email,
         message: this.message
       });
     } else {
-      // Update
       this.contact[this.editIndex] = {
         name: this.name,
         email: this.email,
@@ -34,7 +64,6 @@ export class AdminContact {
       this.editIndex = null;
     }
 
-    // Clear inputs
     this.name = '';
     this.email = '';
     this.message = '';
